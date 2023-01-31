6ef1f4e7e08a69b1d96973977b7d12bdc511b5d8
Emin Kaya <kaya075@gmail.com>
2023-01-31 20:55:16 +0300

    TestCase2 started

---
+++ src/test/java/tests/testCase2_LoginUserWithCorrectEmailAndPassword/testCase2_LoginUserWithCorrectEmailAndPassword_Emin.java
@@ -0,0 +1,7 @@
+package tests.testCase2_LoginUserWithCorrectEmailAndPassword;
+
+public class testCase2_LoginUserWithCorrectEmailAndPassword_Emin {
+    public static void main(String[] args) {
+
+    }
+}
