678b4c26a2a5eb0dd813059261efe1ac42d0ae23
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-02-01 14:03:50 +0300

    New test cases have been uploaded

--- src/test/java/tests/testCase4_LogoutUser/TestCase4_LogoutUser_Mustafa.java
+++ src/test/java/tests/testCase4_LogoutUser/TestCase4_LogoutUser_Mustafa.java
@@ -1,4 +1,15 @@
 package tests.testCase4_LogoutUser;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import utilities.ConfigurationReader;
+
+import java.util.concurrent.TimeUnit;
+
 /*Test Case 4: Logout User
 1. Launch browser
 2. Navigate to url 'http://automationexercise.com'
@@ -11,5 +22,22 @@ package tests.testCase4_LogoutUser;
 9. Click 'Logout' button
 10. Verify that user is navigated to login page*/
 public class TestCase4_LogoutUser_Mustafa {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+        driver.get("http://automationexercise.com");
+
+        Assert.assertEquals(driver.getTitle(),"Automation Exercise","Home page is not visible successfully.");
+
+        //Click on 'Signup / Login' button
+        driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
+
+        WebElement loginToYourAccount= driver.findElement(By.cssSelector("div.login-form>h2"));
+        boolean loginToYourAccountIsVisible=loginToYourAccount.isDisplayed();
+        Assert.assertTrue(loginToYourAccountIsVisible,"Login to your account is not visible");
+
+    }
 }
