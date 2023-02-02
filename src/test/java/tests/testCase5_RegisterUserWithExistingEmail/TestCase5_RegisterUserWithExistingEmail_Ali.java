1d68e060c164f44dc7b68567f949d8ab2af044f6
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-03 01:21:21 +0300

    TestCase5_RegisterUserWithExistingEmail_Ali

---
+++ src/test/java/tests/testCase5_RegisterUserWithExistingEmail/TestCase5_RegisterUserWithExistingEmail_Ali.java
@@ -0,0 +1,19 @@
+package tests.testCase5_RegisterUserWithExistingEmail;
+
+import org.testng.annotations.Test;
+
+public class TestCase5_RegisterUserWithExistingEmail_Ali {
+    @Test
+    public void registerUserWithExistingEmail_test() {
+        //Test Case 5: Register User with existing email
+        //1. Launch browser
+        //2. Navigate to url 'http://automationexercise.com'
+        //3. Verify that home page is visible successfully
+        //4. Click on 'Signup / Login' button
+        //5. Verify 'New User Signup!' is visible
+        //6. Enter name and already registered email address
+        //7. Click 'Signup' button
+        //8. Verify error 'Email Address already exist!' is visible
+    }
+
+}
