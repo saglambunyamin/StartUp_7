97565b8a4d0fb07d2067236d3cab910161af4b26
Emin Kaya <kaya075@gmail.com>
2023-02-02 22:01:44 +0300

    TestCase3 is finished

---
+++ src/test/java/tests/testCase4_LogoutUser/TestCase4_LogoutUser_Emin.java
@@ -0,0 +1,77 @@
+package tests.testCase4_LogoutUser;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import reusableMethods.BrowserUtilities;
+import utilities.Driver;
+import utilities.WebDriverFactory;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestCase4_LogoutUser_Emin {
+
+    WebDriver driver;
+
+    @Test
+    public void logoutUser_Test(){
+
+        //1. Launch browser
+        driver= WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        //2. Navigate to url 'http://automationexercise.com'
+        driver.get("http://automationexercise.com");
+
+        //3. Verify that home page is visible successfully
+        String expectedTitle= "Automation Exercise";
+        String actualTitle= Driver.getDriver().getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle,"Title is not visible successfully");
+
+        BrowserUtilities.sleep(2);
+
+        //4. Click on 'Signup / Login' button
+        driver.findElement(By.xpath("//li[4]")).click();
+
+        //5. Verify 'Login to your account' is visible
+       WebElement loginAccount= driver.findElement(By.xpath("//h2[.='Login to your account']"));
+       Assert.assertTrue(loginAccount.isDisplayed(),"Login to your account is not visible");
+
+        //6. Enter correct email address and password
+        Faker faker = new Faker();
+        WebElement emailData= driver.findElement(By.xpath("//input[@data-qa='login-email']"));
+        String incorrectEmail = faker.internet().emailAddress();
+        emailData.sendKeys(incorrectEmail);
+
+        BrowserUtilities.sleep(2);
+
+        WebElement passwordData= driver.findElement(By.xpath("//input[@data-qa='login-password']"));
+        String incorrectPassword= faker.numerify("#############");
+        passwordData.sendKeys(incorrectPassword);
+
+        BrowserUtilities.sleep(2);
+
+        //7. Click 'login' button
+        WebElement loginButton= driver.findElement(By.xpath("//button[@data-qa='login-button']"));
+        loginButton.click();
+
+        //8. Verify that 'Logged in as username' is visible
+
+        //9. Click 'Logout' button
+
+        //10. Verify that user is navigated to login page
+
+
+
+
+
+
+        driver.close();
+
+
+    }
+}
