bd9759caab680c55617d854756890a797d1f9f25
MustafaAliCetinkaya <macetinkaya.03@gmail.com>
2023-02-04 23:17:35 +0300

    TestCase8_VerifyTestCasesPage passed successfully

--- src/test/java/tests/testCase8_VerifyAllProductsAndProductDetailPage/TestCase8_VerifyAllProductsAndProductDetailPage_Mustafa.java
+++ src/test/java/tests/testCase8_VerifyAllProductsAndProductDetailPage/TestCase8_VerifyAllProductsAndProductDetailPage_Mustafa.java
@@ -1,4 +1,4 @@
-package tests.testCase7_VerifyTestCasesPage;
+package tests.testCase8_VerifyAllProductsAndProductDetailPage;
 
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
@@ -7,13 +7,18 @@ import reusableMethods.BrowserUtilities;
 import utilities.ConfigurationReader;
 import utilities.Driver;
 
-/*Test Case 7: Verify Test Cases Page
+/*Test Case 8: Verify All Products and product detail page
 1. Launch browser
 2. Navigate to url 'http://automationexercise.com'
 3. Verify that home page is visible successfully
-4. Click on 'Test Cases' button
-5. Verify user is navigated to test cases page successfully*/
-public class TestCase7_VerifyTestCasesPage_Mustafa {
+4. Click on 'Products' button
+5. Verify user is navigated to ALL PRODUCTS page successfully
+6. The products list is visible
+7. Click on 'View Product' of first product
+8. User is landed to product detail page
+9. Verify that detail is visible: product name, category, price, availability, condition, brand
+*/
+public class TestCase8_VerifyAllProductsAndProductDetailPage_Mustafa {
     @Test
     public void testCase7() {
         Driver.getDriver().get(ConfigurationReader.getProperty("env"));
@@ -21,15 +26,15 @@ public class TestCase7_VerifyTestCasesPage_Mustafa {
         BrowserUtilities.verifyPageTitle(Driver.getDriver(), "Automation Exercise");
 
         AutomationExercisePage pageObject = new AutomationExercisePage();
-        pageObject.testCasesButton.click();
+        pageObject.productButton.click();
 
-        //Close the goddamn ad window
+        //Close the ad window
         Driver.getDriver().switchTo().frame("aswift_5");
         Driver.getDriver().switchTo().frame("ad_iframe");
         Driver.getDriver().findElement(By.cssSelector("div#dismiss-button")).click();
         Driver.getDriver().switchTo().parentFrame();
 
-        BrowserUtilities.verifyPageTitle(Driver.getDriver(),"Automation Practice Website for UI Testing - Test Cases");
+        BrowserUtilities.verifyPageTitle(Driver.getDriver(),"Automation Exercise - All Products");
 
         Driver.closeDriver();
     }
