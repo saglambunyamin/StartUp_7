87e408c900f7a8f36bcd0d408bb56ba0b8b5296a
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-05 00:14:20 +0300

    TestCase6_ContactUsForm_Ali

--- src/test/java/tests/testCase6_ContactUsForm/TestCase6_ContactUsForm_Ali.java
+++ src/test/java/tests/testCase6_ContactUsForm/TestCase6_ContactUsForm_Ali.java
@@ -1,12 +1,21 @@
 package tests.testCase6_ContactUsForm;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import utilities.ConfigurationReader;
+import utilities.Driver;
 
 public class TestCase6_ContactUsForm_Ali {
     @Test
     public void contactUsForm_test() {
         //1. Launch browser
         //2. Navigate to url 'http://automationexercise.com'
+        Driver.getDriver().get(ConfigurationReader.getProperty("env"));
+
+        //3. Verify that home page is visible successfully
+        String expectedTitle = "Automation Exercise";
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle,"Home page is not visible");
 
         //3. Verify that home page is visible successfully
         //4. Click on 'Contact Us' button
@@ -17,6 +26,8 @@ public class TestCase6_ContactUsForm_Ali {
         //9. Click OK button
         //10. Verify success message 'Success! Your details have been submitted successfully.' is visible
         //11. Click 'Home' button and verify that landed to home page successfully
+
+
     }
 }
 
