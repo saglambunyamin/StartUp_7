d0de008bdf8fa4eaec7069e5341e8bf5e74ec793
Tugba Ozkan Erdem <erdmtba@gmail.com>
2023-02-05 23:35:47 +0300

    pdf1

---
+++ src/test/java/tests/testCase6_ContactUsForm/TestCase6_ContactUsForm_Tugba.java
@@ -0,0 +1,18 @@
+package tests.testCase6_ContactUsForm;
+
+public class TestCase6_ContactUsForm_Tugba {
+/*Test Case 6: Contact Us Form
+testCase6_ContactUsForm
+1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Verify that home page is visible successfully
+4. Click on 'Contact Us' button
+5. Verify 'GET IN TOUCH' is visible
+6. Enter name, email, subject and message
+7. Upload file
+8. Click 'Submit' button
+9. Click OK button
+10. Verify success message 'Success! Your details have been submitted successfully.' is visible
+11. Click 'Home' button and verify that landed to home page successfully*/
+
+}
