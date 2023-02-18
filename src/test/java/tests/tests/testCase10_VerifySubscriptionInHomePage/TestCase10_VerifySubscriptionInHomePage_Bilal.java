ad741b12bc891dfc68f75c3453826d835c9c9c3a
Bilal Cengiz <bcengiz_tr@yahoo.com>
2023-02-18 23:34:53 +0300

    TestCase 9

---
+++ src/test/java/tests/tests/testCase10_VerifySubscriptionInHomePage/TestCase10_VerifySubscriptionInHomePage_Bilal.java
@@ -0,0 +1,17 @@
+package tests.tests.testCase10_VerifySubscriptionInHomePage;
+
+import utilities.BrowserUtilities;
+import utilities.ConfigurationReader;
+import utilities.Driver;
+
+public class TestCase10_VerifySubscriptionInHomePage_Bilal {
+
+    //1. Launch browser
+    //2. Navigate to url 'http://automationexercise.com'
+    Driver.
+
+    //4. Scroll down to footer
+    //5. Verify text 'SUBSCRIPTION'
+    //6. Enter email address in input and click arrow button
+    //7. Verify success message 'You have been successfully subscribed!' is visible
+}
