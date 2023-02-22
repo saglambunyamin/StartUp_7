4b14242060377f97f95dfaea5770c14dfd788f32
Ali KARANLIK <alikaranlik3112@gmail.com>
2023-02-22 11:27:26 +0300

    change

--- src/test/java/pages/DarknessPages/SearchProductPage.java
+++ src/test/java/pages/DarknessPages/SearchProductPage.java
@@ -3,7 +3,10 @@ package pages.DarknessPages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import utilities.Driver;
+
+import java.util.List;
 
 public class SearchProductPage {
     public SearchProductPage() {
@@ -23,4 +26,19 @@ public class SearchProductPage {
 
     @FindBy(xpath = "//h2[.='Searched Products']")
     public WebElement searchedProducts;
+
+    @FindBy(xpath = "//div[@class='product-image-wrapper']")
+    public List<WebElement> allSearchProducts;
+
+    public void verifyAllTheProductsRelatedToSearch(){
+        int i=1;
+        for (WebElement eachProduct : allSearchProducts) {
+            System.out.println(i+". product information is below");
+            Assert.assertTrue(eachProduct.isDisplayed(), eachProduct.getText() + " is not visible");
+            System.out.println(eachProduct.getText() + " is visible");
+            System.out.println("-------------");
+            i++;
+        }
+
+    }
 }
